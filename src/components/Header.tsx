83c6d6a470105802b13c328265b8a8c57def3ddf
walde-dev <paninwaldemar@gmail.com>
2022-10-16 01:42:49 +0200

    update header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,12 @@
 export default function Header() {
   return (
-    <header className="flex w-full justify-center border-b-[0.5px] border-gray-600 border-opacity-75 pt-2 pb-4">
+    <header className="flex w-full justify-center border-b-[0.5px] border-gray-600 border-opacity-60 pt-2 pb-4">
       <div className="flex w-full max-w-5xl flex-row items-center justify-between">
-        <span className="font-extralight text-2xl text-white">
-          waldemar.
-          <span className="gradient bg-clip-text text-2xl text-transparent">
-            dev
-          </span>
-        </span>
+        <h1 className="text-lg  ">waldemar.dev</h1>
+        <div className="flex flex-row gap-6 font-extralight">
+          <span className="">My Work</span>
+          <span>Contact</span>
+        </div>
       </div>
     </header>
   );
--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -4,7 +4,7 @@ import Header from "../Header";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="flex min-h-screen flex-col items-center bg-black py-2 ">
+    <div className="flex min-h-screen flex-col items-center bg-black py-2 text-white ">
       <Head>
         <title>waldemar.dev</title>
         <link rel="icon" href="/favicon.ico" />
