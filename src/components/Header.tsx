92157e2294364eb2acc2568e6ff3c46c95f87485
walde-dev <paninwaldemar@gmail.com>
2022-10-16 16:17:41 +0200

    use next/link

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,23 +1,27 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <header className="flex w-full justify-center py-4">
       <div className="flex w-full max-w-5xl flex-row items-center justify-between">
         <h1 className="text-lg">
-          <a className="px-3 py-1 underline-gradient gradient cursor-pointer">waldemar.dev</a>
+          <Link href="/">
+            <a className="underline-gradient gradient cursor-pointer px-3 py-1">
+              waldemar.dev
+            </a>
+          </Link>
         </h1>
         <div className="flex flex-row gap-6 font-extralight ">
-          <a
-            className="underline-gradient-only underline-gradient hover:text-white"
-            href="work"
-          >
-            My Work
-          </a>
-          <a
-            className="underline-gradient-only underline-gradient hover:text-white"
-            href="contact"
-          >
-            Contact
-          </a>
+          <Link href="/work">
+            <a className="underline-gradient-only underline-gradient hover:text-white">
+              My Work
+            </a>
+          </Link>
+          <Link href="/contact">
+            <a className="underline-gradient-only underline-gradient hover:text-white">
+              Contact
+            </a>
+          </Link>
         </div>
       </div>
     </header>
