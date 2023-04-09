b81b02929663cef9089f9e29ab769b885d963cd8
walde-dev <paninwaldemar@gmail.com>
2023-04-09 12:29:54 +0200

    add some meta tags

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,14 +5,19 @@ import Sidebar from "../Sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="relative flex w-screen min-h-screen flex-col items-center bg-[hsl(235,44%,5%)]  text-gray-300 overflow-hidden">
+    <div className="relative flex min-h-screen w-screen flex-col items-center overflow-hidden  bg-[hsl(235,44%,5%)] text-gray-300">
       <Head>
-        <title>waldemar.dev</title>
+        <title>Waldemar Panin - Freelance Full Stack Web Developer</title>
+        <meta
+          name="description"
+          content="I'm a software engineer based in Munich, Germany. I specialize in building & designing exceptional websites & applications"
+        />
+
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
       <Sidebar />
-      <main className="flex w-full min-h-screen flex-1 flex-col items-center py-12 md:mb-24 md:py-0 px-6 md:px-20 text-center">
+      <main className="flex min-h-screen w-full flex-1 flex-col items-center py-12 px-6 text-center md:mb-24 md:py-0 md:px-20">
         {children}
       </main>
       <Footer />
