8f69a5312563e8f31672fdf2bf2c69f838fa237a
walde-dev <paninwaldemar@gmail.com>
2023-07-13 15:38:00 +0200

    opacity on hover

--- src/components/InfoBanner.tsx
+++ src/components/InfoBanner.tsx
@@ -3,7 +3,7 @@ import { Fade } from "react-awesome-reveal";
 export default function InfoBanner() {
   return (
     <Fade big cascade triggerOnce delay={1000}>
-      <div className="flex w-full max-w-[1200px] translate-y-[200%] flex-row items-center justify-between space-x-7 rounded-lg border border-zinc-700 bg-zinc-800 bg-opacity-60 px-6 py-3 text-sm opacity-80 shadow-xl backdrop-blur-lg">
+      <div className="flex w-full max-w-[1200px] translate-y-[200%] flex-row items-center justify-between space-x-7 rounded-lg border border-zinc-700 bg-zinc-800 bg-opacity-60 px-6 py-3 text-sm opacity-80 hover:opacity-100 shadow-xl backdrop-blur-lg">
         <span className=" text-zinc-500">availablity: july 2023 onwards</span>
         <span className="text-base">
           {" "}
