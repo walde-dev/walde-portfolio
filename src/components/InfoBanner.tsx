ef40686be6a920e641df6e7f0635e9491959c0eb
walde-dev <paninwaldemar@gmail.com>
2023-07-18 19:57:15 +0200

    adapt banner

--- src/components/InfoBanner.tsx
+++ src/components/InfoBanner.tsx
@@ -7,13 +7,10 @@ export default function InfoBanner() {
       cascade
       triggerOnce
       delay={1000}
-      className="flex w-full max-w-5xl items-center justify-center"
+      className="flex w-full max-w-5xl justify-center xl:justify-start items-center"
     >
-      <div className="mb-4 flex w-full max-w-[300px] translate-y-[70%] flex-col items-center justify-center space-x-2 space-y-3 rounded-lg border border-zinc-700 bg-opacity-60 bg-gradient-to-r from-zinc-200 via-zinc-300  to-zinc-200 px-1 py-2 text-sm shadow-xl backdrop-blur-lg transition-all duration-150 ease-in-out hover:opacity-100 dark:from-[#242427] dark:via-zinc-800 dark:to-[#242427] dark:opacity-80 sm:mb-0 sm:px-3 md:max-w-[600px] md:translate-y-[200%] md:flex-row md:justify-between md:space-x-7 md:space-y-0 md:px-6 md:py-3 lg:max-w-full">
-        <span className=" text-zinc-600 dark:text-zinc-500 md:inline-flex">
-          availablity: august 2023 onwards
-        </span>
-        <span className="text-center text-base sm:text-left">
+      <div className="mb-4 flex w-full max-w-[300px] translate-y-[70%] flex-col items-center space-x-2 space-y-3 rounded-lg border border-zinc-700 bg-opacity-60 bg-gradient-to-r from-zinc-200 via-zinc-300  to-zinc-200 px-3 py-2 text-sm shadow-xl backdrop-blur-lg transition-all duration-150 ease-in-out hover:opacity-100 dark:from-[#242427] dark:via-zinc-800 dark:to-[#242427] dark:opacity-80 sm:mb-0 sm:px-3 md:max-w-[600px] md:translate-y-[200%] md:flex-row md:justify-start md:space-x-3 md:space-y-0 md:px-6 md:py-3 lg:max-w-fit">
+        <span className="text-center md:text-left text-lg">
           {" "}
           🚀 currently looking for 1-2 extra projects, ui/ux & frontend work
         </span>
