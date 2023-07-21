c524f5f3c02bc60d51e1fa95a5e0d069df1d461c
walde-dev <paninwaldemar@gmail.com>
2023-07-21 16:49:41 +0200

    add spin to loader

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -65,7 +65,9 @@ export default function ContactForm() {
       <form
         onSubmit={handleSubmit(onSubmit)}
         className={`${
-          emailSent && !rateLimited ? "invisble opacity-0" : "visible opacity-100"
+          emailSent && !rateLimited
+            ? "invisble opacity-0"
+            : "visible opacity-100"
         } flex w-full flex-col space-y-4 transition-all duration-100 ease-in`}
       >
         <div className="flex flex-col justify-center space-y-2">
@@ -117,6 +119,7 @@ export default function ContactForm() {
               viewBox="0 0 24 24"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
+              className="animate-spin"
             >
               <path
                 d="M12 3C16.9706 3 21 7.02944 21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3Z"
